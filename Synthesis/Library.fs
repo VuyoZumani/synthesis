3a2b0fd0d8aa79870de76fcb7bf31fb6abc20374
VuyoZumani <vuyolwethu.zumani@gmail.com>
2019-03-07 19:41:06 +0200

    Modified synthesis up to the minmax function

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,34 +1,61 @@
 ﻿module Synthesis
 
-let abelar _ =
-    failwith "Not implemented"
+let abelar n =  n>12 &&  n<3097 && n%12=0 
+failwith "Not implemented"
 
-let area _ _ =
-    failwith "Not implemented"
+let area bases height = 
+    match bases<0.0 || height<0.0 with
+    |true->failwith "An exception is thrown either of the base or height are negative"
+    |false-> 0.5*bases*height
 
-let zollo _ =
-    failwith "Not implemented"
 
-let min _ _ =
-    failwith "Not implemented"
+let zollo n =
+    match n<0 with
+    |true->n*(-1)
+    |_->2*n
+failwith "Not implemented"
 
-let max _ _ =
-    failwith "Not implemented"
+let min a b =
+   match a<b with
+   |true->a
+   |false->b
+failwith "Not implemented"
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let max a b =
+    match a<b with
+    |true->b
+    |false->a
+failwith "Not implemented"
 
-let toTime _ =
-    failwith "Not implemented"
+let ofTime h m s = 
+    (3600*h)+60*m+s
+failwith "Not implemented"
 
-let digits _ =
-    failwith "Not implemented"
+let toTime secs =
+    let hours= secs/3600
+    let mins=(secs%3600)/60
+    let seconds= (secs%60)
+    match hours<0||mins<0||seconds<0 with
+    |true->0,0,0
+    |false-> hours,mins,seconds
+failwith "Not implemented"
 
-let minmax _ =
-    failwith "Not implemented"
+let digits n =
+    let start=10
+    let rec countDigit v acc=
+        match n/v=0 with
+        |true->acc
+        |false->countDigit (v*10) (acc+1)
+    countDigit start 1
+failwith "Not implemented"
 
-let isLeap _ =
-    failwith "Not implemented"
+let minmax (a,b,c,d) =min (min a b) (min c d),max (max a b) (max c d)
+failwith "Not implemented"
+
+let isLeap n =
+    match n<1582 with
+    |true->failwith "An exception has been thrown"
+    |false->(n%4=0)&&(n%100=0)||(n%400=0)
 
 let month _ =
     failwith "Not implemented"
